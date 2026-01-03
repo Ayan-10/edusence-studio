a1b3c3549a5b48888cca8a2c2d6e7545cb2f542a
Ayan Sarkar <ayan.sarkar@moveinsync.com>
2026-01-03 12:36:06 +0530

    Initial commit message

---
+++ src/main/java/com/example/edusence_studio/EdusenceStudioApplication.java
@@ -0,0 +1,13 @@
+package com.example.edusence_studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusenceStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdusenceStudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/edusence_studio/EdusenceStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edusence_studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdusenceStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
